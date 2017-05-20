602983e456c0076aeb46a9bbce8405d926a2d114
IceTeaCat <4a3k0040@stust.edu.tw>
2017-05-20 15:50:38 +0800

    第九次同步
    修正火球融合問題

---
+++ MagicMaster/Assets/FireBallTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBallTest : MonoBehaviour
+{
+    public GameObject FireBall_Obj;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel("SkillTest");
+            
+        }
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Fireball")
+        {
+            GameObject FBT = Instantiate(FireBall_Obj, transform.position, transform.rotation) as GameObject;
+            float temp = (other.transform.rotation.y * 180 + transform.rotation.y * 180) / 2;
+            FBT.transform.rotation=Quaternion.Euler(0, temp, 0);
+
+            print("A:"+other.transform.rotation.y + " B:" + transform.rotation.y + " Temp:" + temp);
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
--- MagicMaster/Assets/Scripts/Skill/Fusion.cs
+++ MagicMaster/Assets/Scripts/Skill/Fusion.cs
@@ -4,160 +4,72 @@ using System.Collections;
 //融合
 public class Fusion : MonoBehaviour
 {
+    GameObject OtherFireBall;
+    GameObject MyFireBall;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Fireball")
         {
-            GameObject OtherFireBall = other.gameObject;
-            GameObject MyFireBall = this.gameObject;
+            OtherFireBall = other.gameObject;
+            MyFireBall = this.gameObject;
 
-            //目標如果不是濺散火球
-            if (OtherFireBall.GetComponent<Spatter>() == null)
+            if (OtherFireBall.GetComponent<FireBall>().Team == MyFireBall.GetComponent<FireBall>().Team)
             {
-                print("合體");
-                if (OtherFireBall.GetComponent<Fusion>() != null)
+                //目標如果不是濺散火球
+                if (OtherFireBall.GetComponent<Spatter>() == null)
                 {
-                    if (OtherFireBall.GetComponent<FireBall>().BornTime > MyFireBall.GetComponent<FireBall>().BornTime)
+                    print("合體");
+                    //目標如果也是合成火球
+                    if (OtherFireBall.GetComponent<Fusion>() != null)
                     {
-                        Destroy(OtherFireBall.GetComponent<Fusion>());
-                        print("移除融合腳本");
-                        //計算平均角度
-                        if (MyFireBall.transform.rotation.eulerAngles.y > 180 || OtherFireBall.transform.rotation.eulerAngles.y > 180)
+                        if (OtherFireBall.GetComponent<FireBall>().BornTime > MyFireBall.GetComponent<FireBall>().BornTime)
                         {
-                            float temp = 0;
-                            if (MyFireBall.transform.rotation.eulerAngles.y > 180)
-                            {
-                                temp = MyFireBall.transform.rotation.eulerAngles.y - 360;
-
-                                if (OtherFireBall.GetComponent<FireBall>().FireLevel == 1 && MyFireBall.GetComponent<FireBall>().FireLevel == 1)
-                                {
-                                    print("合成中火球");
-                                    PhotonNetwork.Instantiate("Fireball_mid", transform.position, Quaternion.Euler(0, (OtherFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                                }
-                                else
-                                {
-                                    print("合成大火球");
-                                    PhotonNetwork.Instantiate("Fireball_big", transform.position, Quaternion.Euler(0, (OtherFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                                }
-
-                            }
-                            else if (OtherFireBall.transform.rotation.eulerAngles.y > 180)
-                            {
-                                temp = OtherFireBall.transform.rotation.eulerAngles.y - 360;
-                                if (OtherFireBall.GetComponent<FireBall>().FireLevel == 1 && MyFireBall.GetComponent<FireBall>().FireLevel == 1)
-                                {
-                                    print("合成中火球");
-                                    PhotonNetwork.Instantiate("Fireball_mid", transform.position, Quaternion.Euler(0, (MyFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                                }
-                                else
-                                {
-                                    print("合成大火球");
-                                    PhotonNetwork.Instantiate("Fireball_big", transform.position, Quaternion.Euler(0, (MyFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    print("00");
-                    //計算平均角度
-                    if (MyFireBall.transform.rotation.eulerAngles.y > 180 || OtherFireBall.transform.rotation.eulerAngles.y > 180)
-                    {
-                        print("11");
-                        float temp = 0;
-                        if (MyFireBall.transform.rotation.eulerAngles.y > 180)
-                        {
-                            temp = MyFireBall.transform.rotation.eulerAngles.y - 360;
-                            print("AA");
-                            if (OtherFireBall.GetComponent<FireBall>().FireLevel == 1 && MyFireBall.GetComponent<FireBall>().FireLevel == 1)
-                            {
-                                print("合成中火球");
-                                PhotonNetwork.Instantiate("Fireball_mid", transform.position, Quaternion.Euler(0, (OtherFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                            }
-                            else
-                            {
-                                print("合成大火球");
-                                PhotonNetwork.Instantiate("Fireball_big", transform.position, Quaternion.Euler(0, (OtherFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                            }
-
-                        }
-                        else if (OtherFireBall.transform.rotation.eulerAngles.y > 180)
-                        {
-                            print("BB");
-                            temp = OtherFireBall.transform.rotation.eulerAngles.y - 360;
-                            if (OtherFireBall.GetComponent<FireBall>().FireLevel == 1 && MyFireBall.GetComponent<FireBall>().FireLevel == 1)
-                            {
-                                print("合成中火球");
-                                PhotonNetwork.Instantiate("Fireball_mid", transform.position, Quaternion.Euler(0, (MyFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                            }
-                            else
-                            {
-                                print("合成大火球");
-                                PhotonNetwork.Instantiate("Fireball_big", transform.position, Quaternion.Euler(0, (MyFireBall.transform.rotation.eulerAngles.y + temp) / 2, 0), 0);
-                            }
-                        }
-                    }
-                    else
-                        print("22");
-                }
-
-
-                Destroy(OtherFireBall);
-                Destroy(MyFireBall);
-                print("CC");
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        if (other.tag == "Fireball")
-        {
-            if (other.GetComponent<FireBall>().FireLevel == GetComponent<FireBall>().FireLevel)
-            {
-                print("合體");
-                if (other.GetComponent<FireBall>().BornTime > GetComponent<FireBall>().BornTime)
-                {
-                    Destroy(other.gameObject);
-                    if (gameObject.transform.rotation.eulerAngles.y > 180 || other.transform.rotation.eulerAngles.y > 180)
-                    {
-                        float temp = 0;
-                        if (gameObject.transform.rotation.eulerAngles.y > 180)
-                        {
-                            temp = gameObject.transform.rotation.eulerAngles.y - 360;
-                            Instantiate(Fireball_big, transform.position, Quaternion.Euler(0, (other.transform.rotation.eulerAngles.y + temp) / 2, 0));
-                        }
-                        else if (other.transform.rotation.eulerAngles.y > 180)
-                        {
-                            temp = other.transform.rotation.eulerAngles.y - 360;
-                            Instantiate(Fireball_big, transform.position, Quaternion.Euler(0, (gameObject.transform.rotation.eulerAngles.y + temp) / 2, 0));
+                            Destroy(OtherFireBall.GetComponent<Fusion>());
+                            CreateFireBall();
                         }
                     }
                     else
                     {
-                        Instantiate(Fireball_big, transform.position, Quaternion.Euler(0, (other.transform.rotation.eulerAngles.y + gameObject.transform.rotation.eulerAngles.y) / 2, 0));
+                        CreateFireBall();
                     }
-                    Destroy(gameObject);
+                    Destroy(OtherFireBall);
+                    Destroy(MyFireBall);
                 }
             }
         }
-        */
-
-
-
-
     }
+
+
+    void CreateFireBall()
+    {
+        GameObject FB;
+        float temp=0;
+        float a = MyFireBall.transform.rotation.eulerAngles.y;
+        float b= OtherFireBall.transform.rotation.eulerAngles.y;
+
+        if (OtherFireBall.GetComponent<FireBall>().FireLevel == 1 && MyFireBall.GetComponent<FireBall>().FireLevel == 1)
+        {
+            print("合成中火球");
+            FB = PhotonNetwork.Instantiate("Fireball_mid", transform.position, transform.rotation, 0);
+        }
+        else
+        {
+            print("合成大火球");
+            FB = PhotonNetwork.Instantiate("Fireball_big", transform.position, transform.rotation, 0);
+        }
+
+        if (Mathf.Abs(a - b) >= 180)
+            temp = (a + b) / 2 + 180;
+        else
+            temp = (a + b) / 2;
+
+        print("a:" + a + " b:" + b + " temp:" + temp);
+        FB.transform.rotation = Quaternion.Euler(0, temp, 0);
+        FB.GetComponent<FireBall>().Team = MyFireBall.GetComponent<FireBall>().Team;
+  
+    }
+    
 
 
 
